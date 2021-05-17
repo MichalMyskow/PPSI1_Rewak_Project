b92a339d24a6126e3e3d7c1e56186a566605bb54
sbacanski <s.bacanski@gmail.com>
2021-05-17 15:01:44 +0200

    zmiany na szybko

--- front_end/src/components/ArtInfo/ArtInfo.js
+++ front_end/src/components/ArtInfo/ArtInfo.js
@@ -1,34 +1,37 @@
-import { Link } from 'react-router-dom';
-import { Button } from '../../globalStyles';
-import { InfoSec, Container, InfoRow, InfoColumn, TextWrapper, Heading, Subtitle, ImgWrapper, Img } from './ArtInfo.elements';
+import {
+	InfoSec,
+	Container,
+	InfoRow,
+	InfoColumn,
+	TextWrapper,
+	Heading,
+	Subtitle,
+	ImgWrapper,
+	Img
+} from './ArtInfo.elements';
 
-const ArtInfo = ({title, content, photoLink}) => {
-    return (
-        <>
-            <InfoSec>
-                <Container>
-                    <InfoRow>
-                        <InfoColumn>
-                            <TextWrapper>
-                                <Heading >{title}</Heading>
-                                <Subtitle>{content}</Subtitle>
-                                <Link to='/'>
-                                    <Button>
-                                        Przycisk
-                                    </Button>
-                                </Link>
-                            </TextWrapper>
-                        </InfoColumn>
-                        <InfoColumn>
-                            <ImgWrapper>
-                                <Img src={photoLink} alt='Project_photo' />
-                            </ImgWrapper>
-                        </InfoColumn>
-                    </InfoRow>
-                </Container>
-            </InfoSec>
-        </>
-    );
-}
+const ArtInfo = ({ title, content, photoLink }) => {
+	return (
+		<>
+			<InfoSec>
+				<Container>
+					<InfoRow>
+						<InfoColumn>
+							<TextWrapper>
+								<Heading>{title}</Heading>
+								<Subtitle>{content}</Subtitle>
+							</TextWrapper>
+						</InfoColumn>
+						<InfoColumn>
+							<ImgWrapper>
+								<Img src={photoLink} alt='Project_photo' />
+							</ImgWrapper>
+						</InfoColumn>
+					</InfoRow>
+				</Container>
+			</InfoSec>
+		</>
+	);
+};
 
 export default ArtInfo;
--- front_end/src/components/Slider/Slider.js
+++ front_end/src/components/Slider/Slider.js
@@ -1,114 +1,116 @@
 import React, { useState, useEffect } from 'react';
 import {
-    SliderSection,
-    SliderWrapper,
-    SliderSlide,
-    SliderSlider,
-    SliderImg,
-    SliderContent,
-    SliderBtnLink,
-    Arrow,
-    SliderButtons,
-    PrevArrow,
-    NextArrow
+	SliderSection,
+	SliderWrapper,
+	SliderSlide,
+	SliderSlider,
+	SliderImg,
+	SliderContent,
+	SliderBtnLink,
+	Arrow,
+	SliderButtons,
+	PrevArrow,
+	NextArrow
 } from './Slider.elements';
 
 const Slider = () => {
-    const [current, setCurrent] = useState(0);
-    const [posts, setPosts] = useState([]);
-    const length = posts.length;
+	const [current, setCurrent] = useState(0);
+	const [posts, setPosts] = useState([]);
+	const length = posts.length;
 
-    useEffect(() => {
-        const url1 = 'http://blogapi.local/api/posts?page=1';
+	useEffect(() => {
+		const url1 = 'http://blogapi.local/api/posts?page=1';
 
-        fetch(url1, {
-            method: 'GET',
-            headers: {
-                'Content-Type': 'application/json',
-                // 'Authorization': 'BEARER '+ localStorage.getItem('JWT'),
-                'Accept': 'application/json'
-            },
-            credentials: 'include'
-        }).then(resp => resp.json())
-            .then(resp => setPosts(resp))
-            // .then(resp => console.log(resp))
-    }, []);
+		fetch(url1, {
+			method: 'GET',
+			headers: {
+				'Content-Type': 'application/json',
+				// 'Authorization': 'BEARER '+ localStorage.getItem('JWT'),
+				Accept: 'application/json'
+			},
+			credentials: 'include'
+		})
+			.then(resp => resp.json())
+			.then(resp => setPosts(resp));
+		// .then(resp => console.log(resp))
+	}, []);
 
-    const nextSlide = () => {
-        setCurrent(current === length - 1 ? 0 : current + 1);
-    }
+	const nextSlide = () => {
+		setCurrent(current === length - 1 ? 0 : current + 1);
+	};
 
-    const prevSlide = () => {
+	const prevSlide = () => {
+		setCurrent(current === 0 ? length - 1 : current - 1);
+	};
 
-        setCurrent(current === 0 ? length - 1 : current - 1);
-    }
+	if (!Array.isArray(posts) || posts.length <= 0) {
+		return null;
+	}
 
-    if (!Array.isArray(posts) || posts.length <= 0) {
-        return null;
-    }
+	// return (
+	//     <SliderSection>
+	//         <SliderWrapper>
+	//             {slides.map((slide, index) => {
+	//                 return (
+	//                     <SliderSlide key={index}>
+	//                         {index === current && (
+	//                             <SliderSlider>
+	//                                 <SliderImg src={slide.image} alt={slide.alt} />
+	//                                 <SliderContent>
+	//                                     <h1> {slide.title} </h1>
+	//                                     <p> {slide.description} </p>
+	//                                     <SliderBtnLink to={slide.path} css={`max-width: 160px;`}>
+	//                                         {slide.button_label}
+	//                                         <Arrow />
+	//                                     </SliderBtnLink>
+	//                                 </SliderContent>
+	//                             </SliderSlider>
+	//                         )}
+	//                     </SliderSlide>
+	//                 );
+	//             })}
+	//             <SliderButtons>
+	//                 <PrevArrow onClick={prevSlide} />
+	//                 <NextArrow onClick={nextSlide} />
+	//             </SliderButtons>
+	//         </SliderWrapper>
+	//     </SliderSection>
+	// );
 
-
-
-
-    // return (
-    //     <SliderSection>
-    //         <SliderWrapper>
-    //             {slides.map((slide, index) => {
-    //                 return (
-    //                     <SliderSlide key={index}>
-    //                         {index === current && (
-    //                             <SliderSlider>
-    //                                 <SliderImg src={slide.image} alt={slide.alt} />
-    //                                 <SliderContent>
-    //                                     <h1> {slide.title} </h1>
-    //                                     <p> {slide.description} </p>
-    //                                     <SliderBtnLink to={slide.path} css={`max-width: 160px;`}>
-    //                                         {slide.button_label}
-    //                                         <Arrow />
-    //                                     </SliderBtnLink>
-    //                                 </SliderContent>
-    //                             </SliderSlider>
-    //                         )}
-    //                     </SliderSlide>
-    //                 );
-    //             })}
-    //             <SliderButtons>
-    //                 <PrevArrow onClick={prevSlide} />
-    //                 <NextArrow onClick={nextSlide} />
-    //             </SliderButtons>
-    //         </SliderWrapper>
-    //     </SliderSection>
-    // );
-
-    return (
-        <SliderSection>
-            <SliderWrapper>
-                {posts.map((post, index) => {
-                    return (
-                        <SliderSlide key={index}>
-                            {index === current && (
-                                <SliderSlider>
-                                    <SliderImg src={post.photoLink} alt="PhotoError" />
-                                    <SliderContent>
-                                        <h1> {post.title} </h1>
-                                        <p> {post.subtitle} </p>
-                                        {/* <SliderBtnLink to={slide.path} css={`max-width: 160px;`}>
-                                            {slide.button_label}
-                                            <Arrow /> */}
-                                        {/* </SliderBtnLink> */}
-                                    </SliderContent>
-                                </SliderSlider>
-                            )}
-                        </SliderSlide>
-                    );
-                })}
-                <SliderButtons>
-                    <PrevArrow onClick={prevSlide} />
-                    <NextArrow onClick={nextSlide} />
-                </SliderButtons>
-            </SliderWrapper>
-        </SliderSection>
-    );
-}
+	return (
+		<SliderSection>
+			<SliderWrapper>
+				{posts.map((post, index) => {
+					return (
+						<SliderSlide key={index}>
+							{index === current && (
+								<SliderSlider>
+									<SliderImg src={post.photoLink} alt='PhotoError' />
+									<SliderContent>
+										<h1> {post.title} </h1>
+										<p> {post.subtitle} </p>
+										<SliderBtnLink
+											to={'/article' + (current + 1)}
+											css={`
+												max-width: 160px;
+											`}
+										>
+											Przejdź
+											<Arrow />
+										</SliderBtnLink>
+									</SliderContent>
+								</SliderSlider>
+							)}
+						</SliderSlide>
+					);
+				})}
+				<SliderButtons>
+					<PrevArrow onClick={prevSlide} />
+					<NextArrow onClick={nextSlide} />
+				</SliderButtons>
+			</SliderWrapper>
+		</SliderSection>
+	);
+};
 
 export default Slider;
